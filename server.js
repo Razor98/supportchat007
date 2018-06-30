58b262b1b6c2e1340fa93695a760610e83387599
Razor98 <34012779+Razor98@users.noreply.github.com>
2018-06-30 15:26:42 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const COMMANDS = {
 
 wss.on('connection', function connection(ws) {
     var id = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
-    if (clients.indexOf(id) == -1) {
+    if (clients.indexOf(id) != -1) {
         clients.push(ws);
         ws.send('AUTH_OK ' + id);
         console.log("new connection " + id);
