2964720c3eda2819546262e69642c19d88501d52
Razor98 <34012779+Razor98@users.noreply.github.com>
2018-06-30 22:28:08 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -50,11 +50,10 @@ wss.on('connection', function connection(ws) {
         if (message.indexOf('id:') != -1) {
             var name = message.split(':')[1];
             users[name] = ws;
-            ws.send(name);
             for (var key in users) {
                 keyname = keyname + '{id:' + users + '}';
             }
-            [name].send('AUTH 2 ' + keyname);
+            ws[name].send('AUTH 2 ' + keyname);
             delete name;
         }
         delete keyname;
