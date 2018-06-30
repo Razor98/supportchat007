eb88e1057b6da38a8e167558c46c61250500f702
Razor98 <34012779+Razor98@users.noreply.github.com>
2018-06-30 18:06:15 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -14,25 +14,33 @@ var clients = [];
 const COMMANDS = {
     eino: 'non'
 }
-function sendinf(message) {
-    for (var i = 0; i < CLIENTS.length; i++) {
-        clients[i].send(message);
+wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
-}
+    return s4() + s4() + '-' + s4();
+};
 wss.on('connection', function connection(ws) {
-    var id = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
-    if (clients.indexOf(id) == -1) {
-        clients.push(ws);
-        ws.send('AUTH_OK ' + id);
-        console.log("new connection " + id);
-    } else {
-        ws.send('AUTH_CLOSE ' + id);  
-        }//если айди совпадают
+    ws.id = wss.getUniqueID();
+
+    wss.clients.forEach(function each(client) {
+        console.log('Client.ID: ' + client.id);
+        ws.send('AUTH_OK ' + client.id);
+    });
+
+  //  var id = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+  //  if (clients.indexOf(id) == -1) {
+ //       clients.push(ws);
+  //      ws.send('AUTH_OK ' + id);
+ //       console.log("new connection " + id);
+ //   } else {
+  //      ws.send('AUTH_CLOSE ' + id);  
+  //      }//если айди совпадают
  
     ws.on('message', function incoming(message) {//если что то пришло
         console.log('message ' + message);
             info = message;
-            sendinf(info);
+           'clients[client.id].send(info);
     });
 
     ws.on('close', function () {
