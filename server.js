807970b7429f6d11a3fa3d80337f6a99190e7a4f
Razor98 <34012779+Razor98@users.noreply.github.com>
2018-09-17 20:16:45 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -343,7 +343,7 @@ wss.on('connection', function connection(ws) {
             }    
             if (keynameuser.indexOf(recipient) != -1) {
                 try {
-                    users[recipient + '_' + protektion_recipient].send('NOTIFICATION FOR {' + sender + '}');
+                    users[recipient + '_' + protektion_recipient].send('NOTIFICATION FOR {' + sender + "_" + protektion_sender + '}');
                     } catch (err) { console.log('error online status user'); }      
                 }
             } catch (err) { console.log('error online status user globaly'); }  
