bc2f6c3cae497ffa67fa8be7381281eaa4eeea7b
Razor98 <34012779+Razor98@users.noreply.github.com>
2018-09-17 19:29:38 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -330,15 +330,11 @@ wss.on('connection', function connection(ws) {
                     keyname = keyname + key;
                 }         
                 if (keyname.indexOf(recipient) != -1) {
-                    try {
                         chats[sender].send('Online_OK');
                         console.log('Online_OK');
-                    } catch (err) { console.log('error online status01'); }
                 } else {
-                    try {
                         chats[sender].send('Online_null');
                         console.log('Online_null');
-                    } catch (err) { console.log('error online status02'); }
                 }
             } catch (err) { 'error online globaly' }
             try {
