e24a7c38203f20ed4a89fd968752ae0b2c9c8af0
Razor98 <34012779+Razor98@users.noreply.github.com>
2018-09-17 19:38:20 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -325,7 +325,7 @@ wss.on('connection', function connection(ws) {
             var sender = message.split('=')[2];
             var keyname;
             var keynameuser;
-            try {
+   
                 for (var key in chats) {
                     keyname = keyname + key;
                 }         
@@ -336,7 +336,7 @@ wss.on('connection', function connection(ws) {
                         chats[sender].send('Online_null');
                         console.log('Online_null');
                 }
-            } catch (err) { 'error online globaly' }
+           
             try {
             for (var krt in users) {
                 keynameuser = keynameuser + krt;
